507c8b6fba8308595d7a5bd991e40f282c8b5e2a
Francisco Lazarte Salazar <202401362@est.umss.edu>
2026-04-06 21:25:31 -0400

    Prueba de conexion con la base de datos

---
+++ prueba.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+import psycopg
+
+load_dotenv()
+
+try:
+    conn = psycopg.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+    print("Conexion exitosa")
+    conn.close()
+except Exception as e:
+    print("Error:", e)
